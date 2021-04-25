79a133f3bd099b54b36a5fa0c2a36d1ededc8c00
adryan <dryan_guns@yahoo.co.id>
2021-04-25 16:47:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose')
+const { MONGODB } = require('./config.js')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+}) 
+
+mongoose.connect(MONGODB, { useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => {
+    console.log('mongodb connect');
+    return server.listen({ port: 5000 })
+})
+.then((res) => {
+    console.log(`server running at ${res.url}`)
+})
