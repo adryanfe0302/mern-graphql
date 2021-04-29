5c084ad209b81828af5757966a759ff9b2466ac0
adryan <dryan_guns@yahoo.co.id>
2021-04-29 22:08:23 +0700

    get post from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
 import Home from '../src/pages/Home'
 import Login from '../src/pages/Login'
 import Register from '../src/pages/Register'
+import MenuBar from '../src/component/MenuBar'
 
 //css
 import 'semantic-ui-css/semantic.min.css'
@@ -11,9 +13,12 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
+      <Container> 
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
     </Router>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { Grid } from 'semantic-ui-react'
+import PostCard from '../component/Postcard'
 
 const Home = () => {
-    return <>
-        welcome home
-    </>
+    const { 
+        loading, 
+        data
+    } = useQuery(FETCH_POST_QUERY)
+    const posts = !loading && data.getPosts
+   
+    return <Grid columns={3}>
+        <Grid.Row className='page-title'>
+            <h1> Recent Post </h1>
+        </Grid.Row>
+        <Grid.Row>
+        {loading ? <h2> loading post... </h2> : (
+            posts && posts.map((post) => (
+                <Grid.Column key={post.id} style={{marginBottom: '30px'}}>
+                    <PostCard post={post} />
+                </Grid.Column>
+            ))
+            )}
+            </Grid.Row>
+    </Grid>
 }
+
+const FETCH_POST_QUERY = gql`
+{
+    getPosts{
+        id 
+        body 
+        createdAt 
+        username
+        comments {
+            id 
+            body 
+            username 
+            createdAt
+        }
+        likes {
+            id 
+            username
+            createdAt
+        }
+        likeCount
+        commentCount
+    }
+}
+`
 
 export default Home
